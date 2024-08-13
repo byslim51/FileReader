c32381d507d59dd4534dca59a00aaf2aea1fd48f
Slim <you@example.com>
2024-08-13 18:52:58 +0500

    ОБНОВЛЕННАЯ версия второго этап Parking

--- src/parkingLastVeriosn/Parking_Last_Version.java
+++ src/parkingLastVeriosn/Parking_Last_Version.java
@@ -23,17 +23,36 @@ public class Parking_Last_Version implements Parkable {
 
     @Override
     public int unPark(Car carID, int hour) {
-        return (Math.abs(hour - parkedCars.remove(carID)) * 50);
+        return (calculateCost(parkedCars.remove(carID), hour));
     }
+
+    public int calculateCost(int startTime, int endTime) {
+        int sum = 0;
+        while (startTime != endTime) {
+            if ((startTime) % 24 >= 22 || (startTime) % 24 <= 6) {
+                sum += 30;
+            } else {
+                sum += 50;
+            }
+
+            startTime += 1;
+
+            if (startTime == 24) {
+                sum += 30;
+                startTime = 1;
+            }
+        }
+        return sum;
+    }
+
 
     public static void main(String[] args) {
         Parking_Last_Version parking = new Parking_Last_Version(3);
         Car car1 = new Car("10");
         Car car2 = new Car("30");
 
-        System.out.println(parking.park(car1, 20));
-        System.out.println(parking.park(car2, 29));
-        System.out.println(parking.unPark(car2, 30));
+        System.out.println(parking.park(car2, 21));
+        System.out.println(parking.unPark(car2, 1));
 
     }
 
